fe75fc4c97f0cd76b45e4c2502603ac42eb30e56
Itzrohit07 <110466806+Itzrohit07@users.noreply.github.com>
2022-11-17 19:03:50 +0530

    Add files via upload

---
+++ 17_th_noember(lab_session)/Car.java
@@ -0,0 +1,22 @@
+//Write a Java program to insert the specified element at the front of a linked list.
+package TestProgram;
+import java.util.LinkedList;
+public class Car {//create a class
+
+	public static void main(String[] args) {//main method create
+			  
+			     LinkedList<String> A_list = new LinkedList<String>();
+			   
+			          A_list.add("BMW");
+			          A_list.add("Lamborgini");
+			          A_list.add("Jaquer");
+			     System.out.println("Original linked list:" + A_list);    
+			 
+			     A_list.offerFirst("BMW");
+			     System.out.println("BMW is the best");
+			     System.out.println("Best linked list:" + A_list);  
+			 }	
+				
+	}
+
+
